cf99a93d3d7f4198e1edafe814a66645d949e8af
SinaSystem <sinasystem47@gmail.com>
2020-06-04 22:13:36 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/viewpagersimple/DataModel.java
@@ -0,0 +1,13 @@
+package com.sriyanksiddhartha.viewpagersimple;
+
+public class DataModel {
+
+	public int imageId;
+
+	public String title;
+
+	public DataModel(int imageId, String title) {
+		this.imageId = imageId;
+		this.title = title;
+	}
+}
